43ae39cebb7699b195a0fcb364dc728bdc96a7d2
nhandora123 <53312984+nhandora123@users.noreply.github.com>
2021-10-10 17:55:22 +0700

    Update template.config.js

--- template.config.js
+++ template.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "templatereactnativetamphuc",
+  placeholderName: "TemplateReactNativeTamPhuc",
 
   // Directory with template
   templateDir: "./template",
